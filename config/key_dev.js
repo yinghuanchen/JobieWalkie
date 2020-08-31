c72c8cce47b00de54fde0b408bf46a9f9bcabd6e
clintchu <clintchu@outlook.com>
2020-08-30 20:35:03 -0700

    Complete UserAuth

--- config/key_dev.js
+++ config/key_dev.js
@@ -1,4 +1,4 @@
-module.exports = {
-    mongoURI: "mongodb+srv://mernUser:647Ys^EE@jobiewalkie.loz0h.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    secretOrKey: "T$p#27W3",
-}
+// module.exports = {
+//     mongoURI: "mongodb+srv://mernUser:647Ys^EE@jobiewalkie.loz0h.mongodb.net/<dbname>?retryWrites=true&w=majority",
+//     secretOrKey: "T$p#27W3",
+// }
