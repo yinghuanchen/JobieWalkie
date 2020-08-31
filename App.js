c2c0191b98e54e77a932c4f5ee3a67b72b25a494
tgrega <zeroyontlg@yahoo.com>
2020-08-31 12:49:25 -0700

    Fix issue with config

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ const mongoose = require("mongoose")
 const bodyParser = require('body-parser')
 const passport = require("passport")
 const users = require("./routes/api/users")
-// const path = require("path")
+const path = require("path")
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("frontend/build"))
-//     app.get("/", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//     })
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"))
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    })
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true })
