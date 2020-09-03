666003cfd782229cae1649622f8bb013c63e1b40
clintchu <clintchu@outlook.com>
2020-09-03 10:53:40 -0700

    Comment Favorites out of JobListingsIndex

--- frontend/src/components/job_listings/job_listing_index.jsx
+++ frontend/src/components/job_listings/job_listing_index.jsx
@@ -1,33 +1,35 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { connect } from "react-redux"
 import { fetchAllJobListings } from "../../actions/job_listing_actions"
-import { fetchAllFavorites } from "../../actions/favorite_actions"
+// import { fetchAllFavorites } from "../../actions/favorite_actions"
 import JobListingItem from "./job_listing_item"
 import '../../stylesheets/job_listings.css'
 
+// const JobListingIndex = ({ favorites, jobListings, fetchAllFavorites, fetchAllJobListings }) => {
 const JobListingIndex = ({ jobListings, fetchAllFavorites, fetchAllJobListings }) => {
 
     useEffect(() => {
         fetchAllJobListings()
     }, [fetchAllJobListings])
 
-    useEffect(() => {
-        fetchAllFavorites()
-    }, [fetchAllFavorites])
+    // useEffect(() => {
+    //     fetchAllFavorites()
+    // }, [fetchAllFavorites])
 
     return (
       <ul className="joblisting-index">
         {jobListings.map((jobListing) => {
-          return (
-            <JobListingItem key={jobListing._id} jobListing={jobListing} />
-          );
+            return (
+                <JobListingItem key={jobListing._id} jobListing={jobListing} />
+            )
         })}
       </ul>
-    );
+    )
 }
 
 const mapSTP = (state) => {
     return {
+        // favorites: state.favorites.data ? state.favorites.data : [],
         jobListings: state.jobListings.data ? state.jobListings.data : []
     }
 }
@@ -35,8 +37,7 @@ const mapSTP = (state) => {
 const mapDTP = (dispatch) => {
     return {
         fetchAllJobListings: () => dispatch(fetchAllJobListings()),
-        fetchAllFavorites: () => dispatch(fetchAllFavorites())
-        // Clint-TODO: Should rename this to fetchUserFavorites 
+        // fetchAllFavorites: () => dispatch(fetchAllFavorites())
     }
 }
 
